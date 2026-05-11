f8d8620445e1c0f95f21f2fd6771d4913649ec37
emil <emil@svansoe.io>
2026-05-11 20:13:14 +0200

    yep

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-## Im going to rewrite this, there are certain parts of it that needs to be refactored.
-I still use it, but that doesn't mean everybody should.
+## I'm going to rewrite this, there are certain parts of it that needs to be refactored.
 
-Ll-connect(Linux l-connect) has almost all the features of L-connect 3
-it's not perfect but it works, if you have any issues or suggestions let me know.
+### Rewrite progress
 
-it currently only supports the UNI HUB AL v2
+In `./kernel/` there is a hwmon driver, that can be used for controlling fan speed. It works with `fancontrol`
 
-dependencies: SDL2, SDL2_ttf(for the ui)
+If you dont want to use fancontrol you can use motherboard sync by doing:
+
+Set `/sys/class/hwmon/hwmon<some number>/pwm*_enable` to 2 for motherboard sync.
+
+No RGB support rn, will add it back in at some point.
+
+UI wont work currently because i havent gotten to it.
--- kernel/Lian_Li_UNI_HUB_ALv2.c
+++ kernel/Lian_Li_UNI_HUB_ALv2.c
@@ -11,6 +11,7 @@
 #define VENDOR_ID  0x0cf2
 #define PRODUCT_ID 0xa104
 
+// maybe change
 #define BUFFER_SIZE 353
 #define FAN_SPEED_REPORT_SIZE 65
 
@@ -21,6 +22,10 @@
 #define SET_SPEED 0x50
 
 #define ALV2_NUM_PORTS 4
+
+#define DISABLE_MODE 0
+#define MANUAL_MODE 1
+#define AUTO_MODE 2
 
 
 static const char * const port_label[] = { 
@@ -38,14 +43,13 @@ struct drv_data {
 	bool valid;
 	unsigned long last_update;
 	u8 pwm[ALV2_NUM_PORTS];
+	u8 enable[ALV2_NUM_PORTS];
 	// u8 mb_sync;
 
 	struct mutex lock;
 
 	u8 buffer[BUFFER_SIZE];
 };
-
-
 
 // This is most likly not correct, but i dont use it and i dont really want to figure it out again so.... TODO
 // static int mb_sync(struct intf_data *drv, int val)
@@ -74,9 +78,10 @@ struct drv_data {
 // 	}
 // }
 
+
 static int send_output_report(struct drv_data *drv, 
-		       const void *data, 
-		       size_t data_size)
+			      const void *data, 
+			      size_t data_size)
 {
 	int ret;
 
@@ -87,6 +92,9 @@ static int send_output_report(struct drv_data *drv,
 	if (data_size < BUFFER_SIZE) 
 		memset(drv->buffer + data_size, 0, BUFFER_SIZE-data_size);
 	
+	// NOTE maybe this should be changed after sniffing traffic it -
+	// - seems to not just be a static buffer size, tho it works :/
+	// - rgb packets are BUFFER_SIZE, fan speed and the others are different
 	ret = hid_hw_raw_request(drv->hid_dev,
 				 REPORT_ID,
 				 drv->buffer,
@@ -96,9 +104,29 @@ static int send_output_report(struct drv_data *drv,
 	return ret < 0 ? ret : 0;
 }
 
+static int set_mb_sync(struct drv_data *drv, int channel, int enable)
+{
+	u8 port;
+	u8 data[7] = {0};
+
+	if (channel < 0 || channel > 3)
+		return -EINVAL;
+	
+	port = channel + 1;
+	
+	// TODO make label magic numbers
+	data[0] = MSG_START;
+	data[1] = 0x10;
+	data[2] = 0x62;
+	data[3] = 8 << port | (enable ? 1 : 0) << (port - 1);
+
+	return send_output_report(drv, &data, sizeof(data));
+}
+
 /*
  * Hub only sends all the fan rpm's at the same time,
  * Meaning there is no way to get a specific fan's speed.
+ * TODO change the timeout period
  */
 static int update_rpm(struct drv_data *drv_data)
 {
@@ -145,12 +173,59 @@ static int set_speed(struct drv_data *drv, int channel, long val)
 	body[3] = speed; 
 
 	ret = send_output_report(drv, &header, sizeof(header));
-	if (ret < 0) return ret;
+	if (ret < 0) 
+		return ret;
 	ret = send_output_report(drv, &body, sizeof(body));
-	if (ret < 0) return ret;
+	if (ret < 0) 
+		return ret;
+
+	return 0;
+}
+
+static int set_pwm_enable(struct drv_data *drv, int channel, long val)
+{
+	int ret;
+
+	mutex_lock(&drv->lock);
+
+	// if val == AUTO_MODE then toggle motherboard sync on else toggle it off
+	// maybe check be for toggling
+	ret = set_mb_sync(drv, channel, val == AUTO_MODE);
+	if (ret != 0)
+		goto out;
+
+	if (val == DISABLE_MODE)
+		ret = set_speed(drv, channel, 255);
+	else if (val == MANUAL_MODE)
+		ret = set_speed(drv, channel, drv->pwm[channel]);
+
+	if (ret != 0)
+		goto out;
+
+	drv->enable[channel] = val;
+out:
+	mutex_unlock(&drv->lock);
+	return ret;
+}
+
+static int set_pwm(struct drv_data *drv, int channel, long val)
+{
+	int ret = 0;
+
+	mutex_lock(&drv->lock);
+
+	// Only set speed if mode is manual 
+	// if mode is not manual we just update the pwm value
+	if (drv->enable[channel] == MANUAL_MODE) {
+		ret = set_speed(drv, channel, val);
+		if (ret != 0) goto out;
+	}
 
 	drv->pwm[channel] = val;
-	return 0;
+
+out:
+	mutex_unlock(&drv->lock);
+	return ret;
 }
 
 static const struct hwmon_channel_info * const uni_hub_alv2_channel_info[] = {
@@ -200,7 +275,7 @@ static umode_t uni_alv2_hwmon_is_visible(const void *data,
 static int uni_alv2_hwmon_read(struct device *dev, enum hwmon_sensor_types type,
 			       u32 attr, int channel, long *val)
 {
-	struct drv_data *data = dev_get_drvdata(dev);
+	struct drv_data *drv = dev_get_drvdata(dev);
 	int ret;
 
 	if (channel < 0 || channel >= ALV2_NUM_PORTS)
@@ -209,13 +284,13 @@ static int uni_alv2_hwmon_read(struct device *dev, enum hwmon_sensor_types type,
 	switch (type) {
 	case hwmon_fan:
 		if (attr == hwmon_fan_input) {
-			mutex_lock(&data->lock);
+			mutex_lock(&drv->lock);
 
-			ret = update_rpm(data);
+			ret = update_rpm(drv);
 			if (ret == 0) 
-				*val = data->rpm[channel];
+				*val = drv->rpm[channel];
 
-			mutex_unlock(&data->lock);
+			mutex_unlock(&drv->lock);
 
 			return ret;
 		}
@@ -223,10 +298,10 @@ static int uni_alv2_hwmon_read(struct device *dev, enum hwmon_sensor_types type,
 	case hwmon_pwm:
 		switch (attr) {
 		case hwmon_pwm_input:
-			*val = data->pwm[channel];
+			*val = drv->pwm[channel];
 			return 0;
 		case hwmon_pwm_enable:
-			*val = 1;
+			*val = drv->enable[channel];
 			return 0;
 		default:
 			break;
@@ -242,8 +317,7 @@ static int uni_alv2_hwmon_write(struct device *dev,
 				enum hwmon_sensor_types type, 
 				u32 attr, int channel, long val)
 {
-	struct drv_data *data = dev_get_drvdata(dev);
-	int ret;
+	struct drv_data *drv = dev_get_drvdata(dev);
 
 	if (channel < 0 || channel >= ALV2_NUM_PORTS)
 		return -EINVAL;
@@ -252,21 +326,16 @@ static int uni_alv2_hwmon_write(struct device *dev,
 	case hwmon_pwm:
 		switch (attr) {
 		case hwmon_pwm_input:
-			mutex_lock(&data->lock);
-			ret = set_speed(data, channel, val);
-			mutex_unlock(&data->lock);
-			return ret;
-		case hwmon_pwm_enable: // TODO
-			if (val != 1) return -EINVAL;
-			return 0;
+			return set_pwm(drv, channel, val);
+		case hwmon_pwm_enable:
+			return set_pwm_enable(drv, channel, val);
 		default:
-			break;
+			return -EOPNOTSUPP;
 		}
 		break;
 	default:
-		break;
+		return -EOPNOTSUPP;
 	}
-	return -EOPNOTSUPP;
 }
 
 static int uni_alv2_hwmon_read_string(struct device *dev,
@@ -335,22 +404,13 @@ static int alv2_hid_probe(struct hid_device *hid_dev, const struct hid_device_id
 	ret = hid_hw_start(hid_dev, HID_CONNECT_HIDRAW);
 	if (ret) return ret;
 
-	// ret = hid_hw_open(hid_dev);
-	// if (ret){
-		// hid_hw_stop(hid_dev);
-		// return ret;
-	// }
-
-	// hid_device_io_start(hid_dev);
 	drv->hwmon = devm_hwmon_device_register_with_info(&hid_dev->dev, "uni_hub_alv2",
 						          drv, &alv2_hwmon_chip_info, NULL);
 
 	if (IS_ERR(drv->hwmon)) {
-		// hid_hw_close(hid_dev);
 		hid_hw_stop(hid_dev);
 		return PTR_ERR(drv->hwmon);
 	}
-	DPRINTF("probe done");
 	return 0;
 }
 
@@ -370,7 +430,6 @@ static struct hid_driver alv2_drv = {
 
 static int __init mod_init(void)
 {
-	DPRINTF("mod_init");
 	return hid_register_driver(&alv2_drv);
 }
 
